ac1b52dbbf70f4e918d7f60910f7e523651f5dd3
buzkent86 <uzkent.burak@gmail.com>
2017-10-22 22:05:57 -0700

    ROI Mapping Finished - One pixel shift fixed

--- tracker.m
+++ tracker.m
@@ -1,6 +1,6 @@
 function [pr_curve] = tracker(base_path, target, target_sz, ...
 	padding, kernel, lambda, output_sigma_factor, interp_factor, cell_size, ...
-	features,cnn_model, ~)
+	features,cnn_model)
 %   Deep Hyperspectral Kernelized/Dual Correlation Filter (DeepHKCF) tracking.
 %   This function implements the pipeline for tracking with the KCF (by
 %   choosing a non-linear kernel) and DCF (by choosing a linear kernel) and
@@ -40,7 +40,7 @@ function [pr_curve] = tracker(base_path, target, target_sz, ...
 
 %   window size, taking padding into account
 	window_sz = floor(target_sz * (1 + padding));
-    
+	
 	%create regression labels, gaussian shaped, with a bandwidth
 	%proportional to target size
 	output_sigma = sqrt(prod(target_sz)) * output_sigma_factor / cell_size;
@@ -53,8 +53,6 @@ function [pr_curve] = tracker(base_path, target, target_sz, ...
 	%store pre-computed cosine window - to avoid distortion due to FFT
 	cos_window = hann_window(size(yf,1))' * hann_window(size(yf,2));	
 	
-	%note: variables ending with 'f' are in the Fourier domain.
-
 	time = 0;  %to calculate FPS
     
     frameCounter = 1; %Frame Index
@@ -63,39 +61,46 @@ function [pr_curve] = tracker(base_path, target, target_sz, ...
 		
         %load HSI Image - Handle - For now keep reading the same image
         imgHandle = matfile([base_path 'Image_' num2str(frame) '.mat']);
-        roi_mp = 4.0;        
-		
-        tic();
+        
+		tic();
         if frameCounter > 1
             
             %Apply Homograpy to Previous Position
             applyHomograpy(target, frameCounter-1);
             
-            %Sample The ROI From the Full Image
-            xCoord = target.x-(window_sz(1)/2)*roi_mp:target.x+(window_sz(1)/2)*roi_mp-1;
-            yCoord = target.y-(window_sz(2)/2)*roi_mp:target.y+(window_sz(2)/2)*roi_mp-1;
-            %Handle Boundaries
+            %Sample The ROI From the Full Image      
+            xCoord = target.x-(window_sz(1)/3)*3:target.x+(window_sz(1)/3)*3-1;
+            yCoord = target.y-(window_sz(2)/3)*3:target.y+(window_sz(2)/3)*3-1;
             xCoord = boundary_handling(xCoord);
             yCoord = boundary_handling(yCoord);
-            %Sample
-            hsi_roi = imgHandle.img(xCoord,yCoord,:);
+            hsi_roi = imgHandle.img(xCoord,yCoord,:);     
+
+            % Extract Deep Features
+            roi_deep_features = conv_features(hsi_roi, features, cell_size, cos_window, cnn_model);
             
             %Sample SubWindows
-            number_rois = 8;
+            number_rois = 4;
             SubWindowsX = round(linspace(1,size(xCoord,2)-window_sz(1),number_rois));
             SubWindowsY = round(linspace(1,size(yCoord,2)-window_sz(2),number_rois));
-            for i = 1:number_rois
+            for i = 1:number_rois % Search Through ROIs
                 for j = 1:number_rois
-            
+                    
+                    %ROI Mapping
+                    x_in = ceil(SubWindowsX(i) * size(roi_deep_features,1) / size(hsi_roi,1));
+                    y_in = ceil(SubWindowsY(j) * size(roi_deep_features,1) / size(hsi_roi,2));
+                    x_end = ceil((SubWindowsX(i)+window_sz(1)) * size(roi_deep_features,1) / size(hsi_roi,1));
+                    y_end = ceil((SubWindowsY(j)+window_sz(2)) * size(roi_deep_features,2) / size(hsi_roi,2));
+                    features_roi = im_resize(roi_deep_features(x_in:x_end,y_in:y_end,:),[window_sz(1) window_sz(2)]);
+                    
+                    % Apply Hanning Window
+            		features_roi = bsxfun(@times, features_roi, cos_window);
+                    
                     %obtain a subwindow for detection at the position from last
-                    xSubWindow = SubWindowsX(i):SubWindowsX(i)+window_sz(1)-1;
-                    ySubWindow = SubWindowsY(j):SubWindowsY(j)+window_sz(2)-1;
-                    SubWindowX{i,j} = xCoord(1) + xSubWindow(end/2); 
-                    SubWindowY{i,j} = yCoord(1) + ySubWindow(end/2); 
-                    roi = hsi_roi(xSubWindow,ySubWindow,:);
+                    SubWindowX{i,j} = xCoord(1) + SubWindowsX(i) + window_sz(1)/2; 
+                    SubWindowY{i,j} = yCoord(1) + SubWindowsY(j) + window_sz(2)/2;
                     
                     %frame, and convert to Fourier domain (its size is unchanged)
-                    zf = fft2(get_features(roi, features, cell_size, cos_window, cnn_model));
+                    zf = fft2(features_roi);
 
                     %calculate response of the classifier at all shifts
                     switch kernel.type
@@ -114,8 +119,7 @@ function [pr_curve] = tracker(base_path, target, target_sz, ...
                     %discussed in the paper). the responses wrap around cyclically.
                     [vert(i,j), horiz(i,j)] = find(response == max(response(:)), 1);
                     confidence(i,j) = max(max(response)); %Confidence of Tracker
-                    dROI{i,j} = roi;
-
+                    
                 end
             end
                 %Shift the tracker to new position
@@ -132,23 +136,22 @@ function [pr_curve] = tracker(base_path, target, target_sz, ...
                 target.y = SubWindowY{iX,iY} + cell_size * [horiz_delta - 1];  
         end
         
-        %Update the KCF with a PF
-        if frameCounter > 1
-            %[target, particles] = bayes_filter(particles, target);
-        else
-            [particles] = initiate_particles(target);
-        end
-
         %obtain a subwindow for training at newly estimated target position
         %Sample The ROI From the Full Image
-        xCoord = target.x-(window_sz(1)/2)*roi_mp:target.x+(window_sz(1)/2)*roi_mp-1;
-        yCoord = target.y-(window_sz(2)/2)*roi_mp:target.y+(window_sz(2)/2)*roi_mp-1;        
-        %Handle Boundaries
+        xCoord = target.x-(window_sz(1)/3)*3:target.x+(window_sz(1)/3)*3-1;
+        yCoord = target.y-(window_sz(2)/3)*3:target.y+(window_sz(2)/3)*3-1;
         xCoord = boundary_handling(xCoord);
         yCoord = boundary_handling(yCoord);
         hsi_roi = imgHandle.img(xCoord,yCoord,:);
-        %Extract Features
-        xf = fft2(get_features(hsi_roi, features, cell_size, cos_window, cnn_model));
+        
+        %Extract Features and Do ROI Mapping
+        roi_deep_features = conv_features(hsi_roi, features, cell_size, cos_window, cnn_model);
+        features_roi = im_resize(roi_deep_features(14:41,14:41,:),[window_sz(1) window_sz(2)]);
+
+        % Apply Hanning Window
+        features_roi = bsxfun(@times, features_roi, cos_window);   
+        
+        xf = fft2(features_roi);
 
         %Kernel Ridge Regression, calculate alphas (in Fourier domain)
         switch kernel.type
@@ -174,9 +177,7 @@ function [pr_curve] = tracker(base_path, target, target_sz, ...
         time = toc();
         results(frameCounter,:) = [target.x target.y frame time];
         frameCounter = frameCounter + 1;
-
     end
-    %close(vObj);
     
     %Compute Precision
     pr_curve = precision_curve(target,results);
